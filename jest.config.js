92da11e643dcf9b86444720aee15f3efda7079bc
Luiz Pereira <66344405+lcarlostpereira@users.noreply.github.com>
2024-12-09 15:10:09 +0000

    makes > clone-tabnews@1.0.0 test > npm run services:up && npm run wait-for-postgres && concurrently -n next,jest --hide next -k -s command-jest 'next dev' 'jest --runInBand'
    > clone-tabnews@1.0.0 services:up
    > docker compose -f infra/compose.yaml up -d
    
    > clone-tabnews@1.0.0 wait-for-postgres
    > node infra/scripts/wait-for-postgres.js
    
    🔴 Aguardando Postgres aceitar conexões
    🟢 Postgres está pronto e aceitando conexões!
    
    [jest] PASS tests/integration/api/v1/status/get.test.js
    [jest] PASS tests/integration/api/v1/migrations/post.test.js
    [jest] PASS tests/integration/api/v1/migrations/get.test.js
    [jest]
    [jest] Test Suites: 3 passed, 3 total
    [jest] Tests:       3 passed, 3 total
    [jest] Snapshots:   0 total
    [jest] Time:        1.801 s, estimated 2 s
    [jest] Ran all test suites.
    [jest] jest --runInBand exited with code 0
    --> Sending SIGTERM to other processes.. more robust with  and

--- jest.config.js
+++ jest.config.js
@@ -8,5 +8,6 @@ const nextJest = require("next/jest")
 const createJestConfig = nextJest()
 const jestConfig = createJestConfig({
   moduleDirectories: ["node_modules", "<rootDir>"],
+  testTimeout: 60000,
 })
 module.exports = jestConfig
