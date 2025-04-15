534b62566cdc0769f6ccd5c1cca525ec81481214
Luiz Pereira <66344405+lcarlostpereira@users.noreply.github.com>
2025-04-15 14:54:46 +0000

    fiz `eslint` linting

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -3,7 +3,7 @@ const { exec } = require("node:child_process")
 function checkPostgres() {
   exec("docker exec postgres-dev pg_isready --host localhost", handleReturn)
 
-  function handleReturn(error, stdout, stder) {
+  function handleReturn(error, stdout) {
     if (stdout.search("accepting connections") === -1) {
       process.stdout.write(".")
       checkPostgres()
